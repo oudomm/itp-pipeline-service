0b607e85edeadf7c9dd517537c33617e737569e1
oudom <oudomphoem@gmail.com>
2026-01-30 10:23:41 +0700

    feat: circuit breaker pattern for service to service without gateway

--- src/main/java/dev/oudom/pipeline/controller/AccountClientController.java
+++ src/main/java/dev/oudom/pipeline/controller/AccountClientController.java
@@ -1,13 +1,16 @@
 package dev.oudom.pipeline.controller;
 
 import dev.oudom.pipeline.client.AccountClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/client/account")
 @RequiredArgsConstructor
@@ -16,7 +19,19 @@ public class AccountClientController {
     private final AccountClient accountClient;
 
     @GetMapping("/secured")
+    @CircuitBreaker(name = "account", fallbackMethod = "getSecuredDataFallback")
     public Map<String, Object> getSecuredData() {
+        log.info("Calling account service...");
         return accountClient.getSecuredData();
+    }
+
+    // Fallback method - must have same return type and parameters + Exception
+    private Map<String, Object> getSecuredDataFallback(Exception ex) {
+        log.error("Account service is unavailable. Reason: {}", ex.getMessage());
+        return Map.of(
+                "status", "SERVICE_UNAVAILABLE",
+                "message", "Account service is temporarily down. Please try again later.",
+                "error", ex.getMessage()
+        );
     }
 }
